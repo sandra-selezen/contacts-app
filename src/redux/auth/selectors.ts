a8064f31beba3967732cb125dd9449ea2509122e
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-11-13 22:22:01 +0200

    Updated store

--- src/redux/auth/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from "../rootReducer";
-
-export const selectIsLoggedIn = (state: RootState) => state.auth.isLoggedIn;
-export const selectUser = (state: RootState) => state.auth.user;
-export const selectIsRefreshing = (state: RootState) => state.auth.isRefreshing;
